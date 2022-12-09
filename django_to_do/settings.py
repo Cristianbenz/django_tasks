07833334222b1932938c44b9c7bbf79b069ac203
Cristian Benitez <91490781+Cristianbenz@users.noreply.github.com>
2022-12-09 10:22:34 -0300

    Update settings.py

--- django_to_do/settings.py
+++ django_to_do/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["djangotasks-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ['https://djangotasks-production.up.railway.app']
