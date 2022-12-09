24e53e1650fc7366d9833f779a63d0ec034d3480
Cristian Benitez <91490781+Cristianbenz@users.noreply.github.com>
2022-12-09 00:46:56 -0300

    Update settings.py

--- django_to_do/settings.py
+++ django_to_do/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangotasks-production.up.railway.app"]
 
 
 # Application definition
